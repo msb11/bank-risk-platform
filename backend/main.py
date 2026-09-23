8bdb0f1605735878192855e44cb1db58aadc8ff0
msb11 <muskansinghbhumihaar21@gmail.com>
2026-09-23 11:26:52 +0530

    Add transaction risk API

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
-print("Backend")
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+    return {"message":"Bank Risk API"}
+
+@app.post("/predict-risk")
+def predict_risk():
+    return {
+        "risk_score":0.8,
+        "decision":"REVIEW"
+    }
